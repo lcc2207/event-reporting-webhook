8d6bcdaea743516ee9db56583ceaa99df91b99ee
lcc2207 <lynn@fidmail.com>
2018-08-01 09:30:10 -0500

    report.py

--- report.py
+++ report.py
@@ -9,13 +9,13 @@ import dateutil.parser
 
 def format_item(doc):
     fields = [doc['timestamp'], doc['eventName']]
-    for gv in ['SCALR_EVENT_SERVER_ID', 
+    for gv in ['SCALR_EVENT_SERVER_ID',
                'SCALR_EVENT_EXTERNAL_IP',
-               'SCALR_EVENT_INTERNAL_IP', 
+               'SCALR_EVENT_INTERNAL_IP',
                'SCALR_EVENT_SERVER_HOSTNAME',
                'SCALR_EVENT_SERVER_TYPE',
                'SCALR_EVENT_CLOUD_LOCATION',
-               ]:
+              ]:
         fields.append(doc['data'].get(gv, ''))
     return ','.join(fields)
 
@@ -29,19 +29,19 @@ start = dateutil.parser.parse(sys.argv[1])
 end = dateutil.parser.parse(sys.argv[2])
 output = sys.argv[3]
 
-query = json.dumps( { "query" : 
-                        { "range" : { 
-                            "timestamp" : { 
-                                "gte" : start.isoformat(), 
-                                "lt" : end.isoformat()
-                                }
-                            }
+query = {"query" :
+              { "range" : {
+                  "timestamp" : {
+                      "gte" : start.isoformat(),
+                      "lt" : end.isoformat()
                         }
-                    })
+                    }
+              }
+        }
 
 print(query)
 
-res = requests.get('http://localhost:9200/_all/scalr-event/_search', data=query)
+res = requests.get('http://localhost:9200/_all/scalr-event/_search', json=query)
 data = json.loads(res.text)
 
 docs = data['hits']['hits']
